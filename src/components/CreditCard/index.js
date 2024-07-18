b1cd12add2b9b08d48bafecf32ea4a212aaee9a0
SaidendarGoudG <gunigantisaidendargoud@gmail.com>
2024-07-18 07:19:52 +0000

    first commit

---
+++ src/components/CreditCard/index.js
@@ -0,0 +1,68 @@
+import {useState} from 'react'
+
+import {
+  MainContainer,
+  CreditCardDetailsContainer,
+  CreditCardHeadingContainer,
+  CreditCardHeading,
+  HorizontalLine,
+  CardContainer,
+  Card,
+  CardNumber,
+  CardholderNameText,
+  CardholderName,
+  PaymentContainer,
+  PaymentMethodContainer,
+  PaymentMethodHeading,
+  Input,
+} from './styledComponents'
+
+const CreditCard = () => {
+  const [cardNumber, setCardNumber] = useState('')
+  const [cardholderName, setCardholderName] = useState('')
+  const cardholderNameUpperCase = cardholderName.toUpperCase()
+
+  const onChangeCardNumber = event => {
+    setCardNumber(event.target.value)
+  }
+
+  const onChangeCardholderName = event => {
+    setCardholderName(event.target.value)
+  }
+
+  return (
+    <MainContainer>
+      <CreditCardDetailsContainer>
+        <CreditCardHeadingContainer>
+          <CreditCardHeading>CREDIT CARD</CreditCardHeading>
+          <HorizontalLine />
+        </CreditCardHeadingContainer>
+        <CardContainer>
+          <Card data-testid="creditCard">
+            <CardNumber>{cardNumber}</CardNumber>
+            <CardholderNameText>CARDHOLDER NAME</CardholderNameText>
+            <CardholderName>{cardholderNameUpperCase}</CardholderName>
+          </Card>
+        </CardContainer>
+      </CreditCardDetailsContainer>
+      <PaymentContainer>
+        <PaymentMethodContainer>
+          <PaymentMethodHeading>Payment Method</PaymentMethodHeading>
+          <Input
+            type="text"
+            placeholder="Card Number"
+            value={cardNumber}
+            onChange={onChangeCardNumber}
+          />
+          <Input
+            type="text"
+            placeholder="Cardholder Name"
+            value={cardholderName}
+            onChange={onChangeCardholderName}
+          />
+        </PaymentMethodContainer>
+      </PaymentContainer>
+    </MainContainer>
+  )
+}
+export default CreditCard
